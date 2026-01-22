2595df313404b64a9a46ce961db4b0f165495a42
KeylimeVI <noa.higuchi@mail.utoronto.ca>
2026-01-22 13:50:46 -0500

    Update fibonacci.c

--- lab3/fibonacci.c
+++ lab3/fibonacci.c
@@ -18,11 +18,13 @@
 
 void fib(int** array, int n) {
     *array = (int*)malloc(sizeof(int) * n);
-    (*array)[0] = 0;
     if (n >= 1) {
+        (*array)[0] = 0;
+    }
+    if (n >= 2) {
         (*array)[1] = 1;
     }
-    for (int i = 2; i <= n; i++) {
+    for (int i = 2; i < n; i++) {
         (*array)[i] = (*array)[i-1] + (*array)[i-2];
     }
     return;
